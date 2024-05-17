b96de5e0553c46bc20398486ec3060b85549a637
AlyAbdelaal <alialsayedali.aa@gmail.com>
2024-05-17 09:48:43 +0300

    update-setup-file

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
+import setuptools
 from setuptools import setup, find_packages
+
+with open('README.md','r',encoding="utf-8") as fh:
+    long_descriotion =  fh.read()
 
 setup(
     name='terminal_progress_bar',
     version='0.1',
-    packages=find_packages(),
-    requires=[]
+    author="Aly Abdelaal",
+    author_email="alialsayedali.aa@gmail.com",
+    description="Print Progress Bar in terminal, cmd, PowerShell, etc...",
+    long_description=long_descriotion,
+    packages=find_packages(where="src"),
+    requires=[],
+    package_dir={"":"src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"        
+    ]
 
 )
